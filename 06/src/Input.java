0943533b11c624542030e0f039fd3e97d80a8c43
Alexandr-Dolgov <aydolgov@gmail.com>
2016-02-02 20:34:42 +0500

    первый тестовый ручной коммит

--- 06/src/Input.java
+++ 06/src/Input.java
@@ -2,5 +2,6 @@ public class Input {
     public static void main(String[] args) {
         //какой-то код по вводу с клавиатуры double
         System.out.println("test");
+        System.out.println("test2");
     }
 }
