cc7b79bc01472763bf5a449a650f7f2b77ffe921
Alexandr-Dolgov <aydolgov@gmail.com>
2016-02-05 21:10:42 +0500

    Input. используем один сканнер много раз, даем хорошие имена переменным

--- 06/src/InputPrimitiveDataType.java
+++ 06/src/InputPrimitiveDataType.java
@@ -3,42 +3,35 @@ import java.util.Scanner;
 public class InputPrimitiveDataType {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         double d = scanner.nextDouble();
         System.out.print("d = ");
         System.out.println(d);
 
-        Scanner scanner1 = new Scanner(System.in);
-        byte b = scanner.nextByte();
-        System.out.print("b = ");
-        System.out.println(b);
+        byte aByte = scanner.nextByte();
+        System.out.print("aByte = ");
+        System.out.println(aByte);
 
-        Scanner scanner2 = new Scanner(System.in);
         short s = scanner.nextShort();
-        System.out.print("d = ");
-        System.out.println(d);
+        System.out.print("s = ");
+        System.out.println(s);
 
-        Scanner scanner3 = new Scanner(System.in);
         int i = scanner.nextInt();
         System.out.print("i = ");
         System.out.println(i);
 
-        Scanner scanner4 = new Scanner(System.in);
         long l = scanner.nextLong();
         System.out.print("l = ");
         System.out.println(l);
 
-        Scanner scanner5 = new Scanner(System.in);
         float f = scanner.nextFloat();
         System.out.print("f = ");
         System.out.println(f);
 
-        Scanner scanner6 = new Scanner(System.in);
-        boolean bl = scanner.nextBoolean();
-        System.out.print("b1 = ");
-        System.out.println(bl);
+        boolean aBoolean = scanner.nextBoolean();
+        System.out.print("aBoolean = ");
+        System.out.println(aBoolean);
 
-        Scanner scanner7 = new Scanner(System.in);
-        //char c = scanner.nextChar ()
-
+        //char c = scanner.nextChar ()  //у класса Scanner нет метода считывающего единственный символ с клавиатуры
     }
 }
