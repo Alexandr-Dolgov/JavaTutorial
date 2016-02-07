20b20c0ae2f8a6843409807b84a761c66011e30c
Alexandr-Dolgov <aydolgov@gmail.com>
2016-02-07 11:14:38 +0500

    улучшение пользовательского интерфейса

--- 07exception/src/SquareSide.java
+++ 07exception/src/SquareSide.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 public class SquareSide {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("введите сторону квадрата");
         try {
             double d = scanner.nextDouble();
             System.out.println("сторона квадрата " + d);
             System.out.println("периметр квадрата " + (d + d) * 2);
             System.out.println("площадь квадрата " + d * d);
-
         } catch (InputMismatchException ex) {
             System.out.println("Не удалось распознать число((");
         }
