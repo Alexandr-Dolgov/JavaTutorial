d5e2f2dc7a4d7c842a504692d8483d8799e57cd6
Alexandr-Dolgov <aydolgov@gmail.com>
2016-02-10 20:08:25 +0500

    Методы классов (примеры)

---
+++ 10methods/src/MethodsReturn.java
@@ -0,0 +1,11 @@
+public class MethodsReturn {
+
+    public static void main(String[] args) {
+        System.out.println(MethodsReturn.calcSquarePerimeter(3) + 1);
+    }
+
+    static int calcSquarePerimeter(int a) {
+        return a * 4;
+    }
+
+}
---
+++ 10methods/src/MethodsVoid.java
@@ -0,0 +1,32 @@
+public class MethodsVoid {
+
+    public static void main(String[] args) {
+        double d = Math.pow(2, 4);
+        System.out.println(d);
+
+        //вызов метода без аргументов
+        MethodsVoid.my();
+
+        //вызов метода принимающего 1 аргумент типа int
+        MethodsVoid.printHello(2);
+
+        //вызов метода принимающего 2 аргумента String, int
+        MethodsVoid.print("Работает метод с двумя аргументами", 3);
+    }
+
+    static void my() {
+        System.out.println("привет из метода my()");
+    }
+
+    static void printHello(int number) {
+        for (int i = 0; i < number; i = i + 1) {
+            System.out.println("hello");
+        }
+    }
+
+    static void print(String s, int number) {
+        for (int i = 0; i < number; i = i + 1) {
+            System.out.println(s);
+        }
+    }
+}
