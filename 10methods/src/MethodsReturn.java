a4b3a54cb84036eb5d60d57e7c026c222bbfec09
Alexandr-Dolgov <aydolgov@gmail.com>
2016-02-11 22:06:05 +0500

    Методы классов (упражнения)

--- 10methods/src/MethodsReturn.java
+++ 10methods/src/MethodsReturn.java
@@ -2,6 +2,8 @@ public class MethodsReturn {
 
     public static void main(String[] args) {
         System.out.println(MethodsReturn.calcSquarePerimeter(3) + 1);
+
+        System.out.println(Rectangle.square(3, 5));
     }
 
     static int calcSquarePerimeter(int a) {
