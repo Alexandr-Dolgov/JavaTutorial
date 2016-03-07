4644ecfbc0f0855831cdcf9a77de38337e744289
Kirill <9868822@gmail.com>
2016-03-07 18:00:38 +0400

    задание Words

--- words/src/App.java
+++ words/src/App.java
@@ -1,143 +1,59 @@
-import jdk.nashorn.internal.ir.WhileNode;
-
 import java.util.Scanner;
+//перейти на switch
+//класс Words инкапсулирующий работу с массивом
+//добавить показ случайного (random)
+//добавить показ случайного среди тех, у кого количество показов наименьшее
+//добавление слова через пользовательский интерфейс
 
-//адание на методы и работу с массивами объектов
-//        Класс Word
-//
-//        с двумя полями
-//private String word
-//private int quantityDisplays
-//
-//        одним конструктором принимающим слово
-//public Word(String word)
-//
-//
-//        Класс App с точкой входа
-//        где будет инициализировать массив объектов типа Word
-//
-//        затем в "бесконечном" цикле while будем считывать с клавиатуры
-//        exit - выход из программы
-//        next - следующее слово
-//        prev - предыдущее слово
-//        что-то другое - некорректный ввод и говорим какой ввод допустим
-//
-//        Если пользователь дошел до конца массива не выбрасывать исключение а продолжать показ с 0 ячейки массива
 
 public class App {
-    /*
-    Программа выполняет:
-    - выбор языка на котором выводить слова (нет в задании)
-    - выводит слова в прямой и обратной последовательности
-    - выводит перевод слова (нет в задании)
-    - при неправильном вводе комкнд выводит подсказку
-    - при выходе из программы показывает статистику
-     */
-
     public static void main(String[] args) {
-        Word[] words = initialization();
-        int index = 0;
-        System.out.println("en - English\n" +
-                "ru - Russian\n");
-
-        //Word word = words [index] ;
-        //System.out.println(word.english);
-
+        Words words = new Words();
         Scanner scanner = new Scanner(System.in);
-        String inputLanguage = scanner.next();
-        for (int i = 0; i < 3; i++) {
-            if (inputLanguage.equals("en")) {
-                System.out.println("exit - выход из программы\n" +
-                        "next - следующее слово\n" +
-                        "prev - предыдущее слово\n" +
-                        "trl - перевод слова\n" +
-                        " ");
-                words[index].displayEn();
-                break;
-            } else if (inputLanguage.equals("ru")) {
-                System.out.println("exit - выход из программы\n" +
-                        "next - следующее слово\n" +
-                        "prev - предыдущее слово\n" +
-                        "trl - перевод слова\n" +
-                        " ");
-                words[index].displayRus();
-                break;
-            } else {
-                System.out.println("некорректный ввод! введите en или ru");
-            }
-            inputLanguage = scanner.next();
-        }
-
+        instruction();
         String input = scanner.next();
+
+
         while (!input.equals("exit")) {
-            if (input.equals("next")) {
-                if (index == words.length - 1) {
-                    index = 0;
-                } else {
-                    index++;
-                }
 
-                if (inputLanguage.equals("en")) {
-                    words[index].displayEn();
-                } else if (inputLanguage.equals("ru")) {
-                    words[index].displayRus();
-                }
-                //words[index].displayEn();
+//            if (input.equals("eng")){//выбор языка
+//                boolean onRus = true;
+//                if (onRus) {
+//                    words.language();
+//                }
+//            }
 
-            } else if (input.equals("trl")) {
-                if (inputLanguage.equals("ru")) {
-                    words[index].displayEn();
-                } else if (inputLanguage.equals("en")) {
-                    words[index].displayRus();
-                }
-                //words[index].displayRus();
-            } else if (input.equals("prev")) {
-                if (index == 0) {
-                    index = words.length - 1;
-                } else {
-                    index--;
-                }
 
-                if (inputLanguage.equals("en")) {
-                    words[index].displayEn();
-                } else if (inputLanguage.equals("ru")) {
-                    words[index].displayRus();
-                }
-                //words[index].displayEn();
 
-            } else {
-                System.out.println("некорректный ввод! введите...\n" +
-                        "exit - выход из программы\n" +
-                        "next - следующее слово\n" +
-                        "prev - предыдущее слово\n" +
-                        "trl - перевод слова");
+            switch (input) {
+
+                case "next":
+                    words.displayNext();
+                    break;
+                case "prev":
+                    words.displayPrev();
+                    break;
+                case "rand":
+                    words.displayRandom();
+                    break;
+                case "rnd":
+                    words.displayRandomNotPopular();
+                    break;
+                default:
+                    System.out.println("некорректный ввод");
+                    instruction();
+                    break;
             }
             input = scanner.next();
-
         }
         System.out.println("программа завершена, вот...");
-        printWords(words);
+        words.print();
     }
 
-
-
-    static void printWords(Word[] words) {
-        for (int i = 0; i < words.length; i++) {
-            System.out.print(i + ": ");
-            words[i].println();
-        }
+    static void instruction() {
+        System.out.println("next - следующее слово");
+        System.out.println("prev - предыдущее слово");
+        System.out.println("rand - случайное слово");
+        System.out.println("exit - выход из программы");
     }
-
-    static Word[] initialization() {
-        Word[] words = {
-                new Word("мир", "world", true),
-                new Word("привет", "hello", false),
-                new Word("кто", "who", true),
-                new Word("что", "what", true),
-                new Word("мед", "honey", false)
-        };
-        return words;
-    }
-
-
 }
--- words/src/Words.java
+++ words/src/Words.java
@@ -1,69 +1,64 @@
-package AppWord;
-
 import java.util.Random;
 
 public class Words {
-
     private Word[] words;
     private int index;
 
     public Words() {
         words = new Word[]{
-                new Word("привет"),
-                new Word("пока"),
-                new Word("объект"),
-                new Word("экземпляр"),
-                new Word("класс")
-        };
+                new Word("мир", "world", true),
+                new Word("привет", "hello", true),
+                new Word("кто", "who", true),
+                new Word("что", "what", true),
+                new Word("мед", "honey", true)};
     }
 
     public void displayNext() {
         index = (index == words.length - 1) ? 0 : index + 1;
-        words[index].display();
+        words[index].displayRus();
     }
 
     public void displayPrev() {
         index = (index == 0) ? words.length - 1 : index - 1;
-        words[index].display();
+        words[index].displayRus();
     }
 
     public void displayRandom() {
         Random random = new Random();
         int randomIndex = random.nextInt(words.length);
-        words[randomIndex].display();
+        words[randomIndex].displayRus();
     }
 
     public void displayRandomNotPopular() {
-        int length = quantityNotPopularWords();
-        Word[] notPopularWords = new Word[length];
+        int lenght = quantityNotPopularWords();
+        Word[] notPopularWords = new Word[lenght];
         int minQuantityDisplays = minQuantityDisplays();
         int j = 0;
         for (int i = 0; i < words.length; i++) {
-            if (words[i].getQuantityDisplays() == minQuantityDisplays) {
+            if (words[i].getQuantityDisplay() == minQuantityDisplays) {
                 notPopularWords[j] = words[i];
             }
         }
         Random random = new Random();
         int randomIndex = random.nextInt(notPopularWords.length);
-        notPopularWords[randomIndex].display();
+        notPopularWords[randomIndex].displayRus();
     }
 
     private int quantityNotPopularWords() {
         int quantity = 0;
         int minQuantityDisplays = minQuantityDisplays();
         for (int i = 0; i < words.length; i++) {
-            if (words[0].getQuantityDisplays() == minQuantityDisplays) {
-                quantity++;
-            }
+            if (words[0].getQuantityDisplay() == minQuantityDisplays) ;
+            quantity++;
         }
         return quantity;
     }
 
     private int minQuantityDisplays() {
-        int minQuantityDisplays = words[0].getQuantityDisplays();
+        int minQuantityDisplays = words[0].getQuantityDisplay();
         for (int i = 1; i < words.length; i++) {
-            if (minQuantityDisplays < words[i].getQuantityDisplays()) {
-                minQuantityDisplays = words[i].getQuantityDisplays();
+            if (minQuantityDisplays < words[i].getQuantityDisplay()) {
+                minQuantityDisplays = words[i].getQuantityDisplay();
             }
         }
         return minQuantityDisplays;
@@ -71,7 +66,7 @@ public class Words {
 
     public void print() {
         for (int i = 0; i < words.length; i++) {
-            System.out.print(i + ": ");
+            System.out.println(i + " : ");
             words[i].println();
         }
     }
