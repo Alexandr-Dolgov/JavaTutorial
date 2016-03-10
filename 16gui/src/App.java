94c7b5cde092d56b05a8f47c4d90b36b288ae26a
Alexandr-Dolgov <aydolgov@gmail.com>
2016-03-10 22:04:05 +0500

    Простейшее GUI приложелие

---
+++ 16gui/src/App.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class App {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setTitle("моя первая программа с графическим интрефейсом пользователя");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+}
