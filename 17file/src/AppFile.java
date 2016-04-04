e4359c93fa5e5fb03877bdc4091e258ed47e4974
Alexandr-Dolgov <aydolgov@gmail.com>
2016-04-04 22:05:33 +0500

    чтение из файла

--- 17file/src/AppFile.java
+++ 17file/src/AppFile.java
@@ -12,18 +12,38 @@ public class AppFile {
         }
         */
 
-        File f = new File("tmp.txt");
+        File f1 = new File("C:\\Data\\Develop\\Students\\30_KirillSokolov\\JavaTutorial\\17file\\src\\t\\t.txt");
+        File f2 = new File("tmp.txt");
 
-        FileReader fr = new FileReader(f);
+        // io = input output
 
-        int i = fr.read();
-        char c = (char) i;
-        i = fr.read();
+        FileReader fr = new FileReader(f1);
 
-        c = (char) fr.read();
-        System.out.println(c);
+        String rus, eng;
+        String s = "";
+        int c = fr.read();
+        int i = 0;
+        int commaIndex = 0;
+        while (c != -1) {   //когда fr.read() возвращает -1 значит файл закончился
+            char ch = (char) c;
+            if (ch == ',') {
+                commaIndex = i;
+            }
+            if (ch == '\r') {
+                rus = s.substring(0, commaIndex);
+                eng = s.substring(commaIndex + 1, i);
+                //word = new Word(rus, eng, false)
+                //dictionary.addWord(new Word(rus, eng, false))
+                s = "";
+            }
+            s = s + ch;
+            c = fr.read(); //считываем код символы
+            i++;
+        }
 
-        System.out.println("test");
+        System.out.println("\ntest");
+
+        //привет,hello,eng
     }
 
 }
