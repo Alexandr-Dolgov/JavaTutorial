458a6cf68638c11115b056f9813c2a3107869d59
Alexandr-Dolgov <aydolgov@gmail.com>
2016-05-16 21:38:10 +0500

    JUnit

---
+++ wordsMvn/src/test/java/com/sokolov/words/model/WordTest.java
@@ -0,0 +1,19 @@
+package com.sokolov.words.model;
+
+import org.junit.Test;
+
+public class WordTest {
+
+    @Test
+    public void equals_X_and_X() {
+        Word word = new Word("привет", "hello", false);
+        org.junit.Assert.assertTrue(word.equals(word));
+    }
+
+    @Test
+    public void equals_X_and_Y() {
+        Word x = new Word("пока", "goodbye", false);
+        Word y = new Word("", "goodbye", false);
+        org.junit.Assert.assertFalse(x.equals(y));
+    }
+}
