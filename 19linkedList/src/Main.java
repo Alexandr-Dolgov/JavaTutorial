83e333773421a94b5edb48a3b69741b82cad536d
Alexandr-Dolgov <aydolgov@gmail.com>
2016-05-19 22:10:15 +0500

    ArrayList, LinkedList

---
+++ 19linkedList/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        LL list = new LL();
+        list.add("a");
+        list.add("b");
+    }
+
+}
---
+++ wordsMvn/src/test/java/com/sokolov/words/model/ArrayListTest.java
@@ -0,0 +1,15 @@
+package com.sokolov.words.model;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+public class ArrayListTest {
+
+    @Test
+    public void test() {
+        ArrayList list = new ArrayList();
+        list.add("qwerty");
+    }
+
+}
